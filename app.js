0812fd13ec21c29b563bec9e89d4151303eb431a
Gustavo Coelho <136639290+Guuzih@users.noreply.github.com>
2024-01-23 16:50:35 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('texto_semTexto').style.display = 'block';
+    document.getElementById('texto_comTexto').style.display = 'none';
+})
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+};
+
+const codificador = () => {
+
+    let texto = document.querySelector('input').value;
+
+    texto = texto.split(" ");
+    let textoCodificado="";
+    for(palavra of texto){
+        
+      for(letra of palavra){
+        if(letra === "a"){
+            textoCodificado += "ai"
+        }
+        else if(letra === "e"){
+            textoCodificado += "enter"
+        }
+        else if(letra === "i"){
+            textoCodificado += "imes"
+        }
+        else if(letra === "o"){
+            textoCodificado += "ober"
+        }
+        else if(letra === "u"){
+            textoCodificado += "ufat"
+        }
+        else{
+            textoCodificado += letra
+        };
+     
+      };
+      textoCodificado += " "
+    };
+    document.getElementById('texto_semTexto').style.display = 'none';
+    document.getElementById('texto_comTexto').style.display = 'block';
+  exibirTextoNaTela("h2", textoCodificado);
+};
+
+const decodificar = () => {
+    let texto = document.querySelector('input').value;
+
+    texto = texto.split(" ");
+    let textoDecodificado = "";
+     for(palavra of texto){
+        let validacao = palavra.includes("ober") || palavra.includes("enter") || palavra.includes("imes") || palavra.includes("ober") || palavra.includes("ufat");
+            while(validacao === true){
+          palavra = palavra.replace(/ai/i, "a");
+          palavra = palavra.replace(/enter/i, "e");
+          palavra = palavra.replace(/imes/i, "i");
+          palavra = palavra.replace(/ober/i, "o");
+          palavra = palavra.replace(/ufat/i, "u");
+    validacao = palavra.includes("ober") || palavra.includes("enter") || palavra.includes("imes") || palavra.includes("ober") || palavra.includes("ufat") || palavra.includes("ai");
+            };
+         textoDecodificado += palavra + " ";
+     };
+
+     document.getElementById('texto_semTexto').style.display = 'none';
+     document.getElementById('texto_comTexto').style.display = 'block';
+    exibirTextoNaTela("h2", textoDecodificado);
+};
